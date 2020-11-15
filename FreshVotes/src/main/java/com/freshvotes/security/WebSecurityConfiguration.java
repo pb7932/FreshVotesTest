f2a41dfaf57fa4ec4583dcde90cfdc5f88644509
pb7932 <pebe1928@gmail.com>
2020-11-15 21:06:48 +0100

    completing a registration page, still unable to fix the encrytion bug

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 
@@ -21,13 +23,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 		return new BCryptPasswordEncoder();
 	}
 	
-	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.userDetailsService(userDetailsService)
 			.passwordEncoder(getPasswordEncoder());
-		
 			
 //		auth.inMemoryAuthentication()
 //			.passwordEncoder(getPasswordEncoder())
--- FreshVotes/src/main/java/com/freshvotes/web/LoginController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/LoginController.java
@@ -1,10 +1,19 @@
 package com.freshvotes.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.freshvotes.domain.User;
+import com.freshvotes.service.UserService;
 
 @Controller
 public class LoginController {
+
+	@Autowired
+	private UserService userService;
 
 	@GetMapping("/login")
 	public String login() {
@@ -12,7 +21,14 @@ public class LoginController {
 	}
 	
 	@GetMapping("/register") 
-	public String register() {
+	public String register(ModelMap model) {
+		model.put("user", new User());
 		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String registerPost(User user) {
+		User savedUser = userService.save(user);
+		return "redirect:/login";
 	}
 }
